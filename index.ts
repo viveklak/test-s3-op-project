9139205a845b45321b8d6d6c6844f0d336aa18aa
Vivek Lakshmanan <vivek@pulumi.com>
2021-04-01 21:44:22 -0700

    Empty stack instead

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+// import * as pulumi from "@pulumi/pulumi";
+// import * as aws from "@pulumi/aws";
+// import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const names = [];
-for (let i = 0; i < 3; i++) {
-    const bucket = new aws.s3.Bucket(`my-bucket-${i}`);
-    names.push(bucket.id);
-}
+// // Create an AWS resource (S3 Bucket)
+// const names = [];
+// for (let i = 0; i < 3; i++) {
+//     const bucket = new aws.s3.Bucket(`my-bucket-${i}`);
+//     names.push(bucket.id);
+// // }
 
-// Export the name of the buckets
-export const bucketNames = names;
+// // Export the name of the buckets
+// export const bucketNames = names;
+
+
+console.log("Empty stack!");
